7dddc2384c7ecf401ca1e03f30aa84b45c1892ef
Tyler Wickline <twick00@gmail.com>
2019-02-13 11:53:25 -0800

    Updates

---
+++ precache-manifest.c25a6df2c4a10d1b367e39f1d68adcac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee529849bd16b9346d3f",
+    "url": "/react-deeply-nested-demo/static/js/runtime~main.ee529849.js"
+  },
+  {
+    "revision": "28b807e81bd9ab56757e",
+    "url": "/react-deeply-nested-demo/static/js/main.28b807e8.chunk.js"
+  },
+  {
+    "revision": "aa6c62d01acaab6188df",
+    "url": "/react-deeply-nested-demo/static/js/2.aa6c62d0.chunk.js"
+  },
+  {
+    "revision": "28b807e81bd9ab56757e",
+    "url": "/react-deeply-nested-demo/static/css/main.353480e5.chunk.css"
+  },
+  {
+    "revision": "ff458e4f25836d2b8f95eb065067d0eb",
+    "url": "/react-deeply-nested-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.feb2b5e85ecf58fa851310e5ff9bbf34.js"
+  "/react-deeply-nested-demo/precache-manifest.c25a6df2c4a10d1b367e39f1d68adcac.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-deeply-nested-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
